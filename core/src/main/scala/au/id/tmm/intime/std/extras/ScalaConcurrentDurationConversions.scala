24a5f09da505b24cd54944228df993284992b4f9
Timothy McCarthy <ebh042@gmail.com>
2021-01-02 10:04:47 +1100

    WiP for syntax

--- core/src/main/scala/au/id/tmm/intime/std/extras/ScalaConcurrentDurationConversions.scala
+++ core/src/main/scala/au/id/tmm/intime/std/extras/ScalaConcurrentDurationConversions.scala
@@ -8,9 +8,9 @@ import scala.concurrent.duration.{
   NANOSECONDS => S_NANOSECONDS,
 }
 
+// TODO these all need docs
 object ScalaConcurrentDurationConversions {
 
-  // TODO scaladoc should explain option
   def jDurationToSDuration(
     jDuration: JDuration,
   ): Either[Errors.JavaTimeDurationTooLargeForScalaConcurrentDurationException, SFiniteDuration] =
@@ -18,23 +18,69 @@ object ScalaConcurrentDurationConversions {
       Right(SFiniteDuration(jDuration.toNanos, S_NANOSECONDS))
     } catch {
       case i: IllegalArgumentException =>
-        Left(Errors.JavaTimeDurationTooLargeForScalaConcurrentDurationException(jDuration)(i))
+        Left(new Errors.JavaTimeDurationTooLargeForScalaConcurrentDurationException(jDuration, i))
       case e: ArithmeticException =>
-        Left(Errors.JavaTimeDurationTooLargeForScalaConcurrentDurationException(jDuration)(e))
+        Left(new Errors.JavaTimeDurationTooLargeForScalaConcurrentDurationException(jDuration, e))
     }
+
+  def jDurationToSDurationTotal(jDuration: JDuration): SDuration = ???
 
   def sFiniteDurationToJDuration(sFiniteDuration: SFiniteDuration): JDuration =
     JDuration.ofNanos(sFiniteDuration.toNanos)
 
-  def sDurationToJDuration(sDuration: SDuration): Option[JDuration] =
+  def sDurationToJDuration(sDuration: SDuration): Either[Errors.ScalaConcurrentDurationIsInfinite, JDuration] =
     sDuration match {
-      case infinite: SDuration.Infinite     => None
-      case sFiniteDuration: SFiniteDuration => Some(sFiniteDurationToJDuration(sFiniteDuration))
+      case infinite: SDuration.Infinite     => Left(new Errors.ScalaConcurrentDurationIsInfinite(infinite))
+      case sFiniteDuration: SFiniteDuration => Right(sFiniteDurationToJDuration(sFiniteDuration))
     }
 
+  def sDurationToJDurationTotal(sDuration: SDuration): JDuration = ???
+
   object Errors {
-    final case class JavaTimeDurationTooLargeForScalaConcurrentDurationException(jDuration: JDuration)(cause: Exception)
+    final class JavaTimeDurationTooLargeForScalaConcurrentDurationException(val jDuration: JDuration, cause: Exception)
         extends Exception(s"Duration $jDuration does not fit within a ${SDuration.getClass.getCanonicalName}", cause)
+
+    final class ScalaConcurrentDurationIsInfinite(val sDuration: SDuration.Infinite)
+      extends Exception(s"Duration $sDuration is infinite and does not fit within a ${classOf[JDuration].getCanonicalName}")
+  }
+
+  object Syntax {
+
+    final class JDurationOps private[std] (jDuration: JDuration) {
+      def toScalaConcurrent: Either[Errors.JavaTimeDurationTooLargeForScalaConcurrentDurationException, SFiniteDuration] = ???
+
+      def toScalaConcurrentUnsafe: SFiniteDuration = ???
+
+      def toScalaConcurrentOrInfinite: SDuration = ???
+    }
+
+    object JDurationOps {
+      trait ToJDurationOps {
+      }
+    }
+
+    final class SDurationOps private(sDuration: SDuration) {
+      def toJava: Either[Errors.ScalaConcurrentDurationIsInfinite, JDuration] = ???
+
+      def toJavaUnsafe: JDuration = ???
+
+      def toJavaTotal: JDuration = ???
+    }
+
+    final class SFiniteDurationOps private[ScalaConcurrentDurationConversions] (sFiniteDuration: SFiniteDuration) {
+      def toJava: JDuration = ???
+    }
+
+    object SDurationOps {
+      trait ToSDurationOps extends LowPriorityToSDurationOps {
+        implicit def toSFiniteDurationOps(sFiniteDuration: SFiniteDuration): SFiniteDurationOps = new SFiniteDurationOps(sFiniteDuration)
+      }
+
+      trait LowPriorityToSDurationOps {
+        implicit def toSDurationOps(sDuration: SDuration): SDurationOps = new SDurationOps(sDuration)
+      }
+    }
+
   }
 
 }
--- scalacheck/src/test/scala/au/id/tmm/intime/std/extras/ScalaConcurrentDurationConversionsSpec.scala
+++ scalacheck/src/test/scala/au/id/tmm/intime/std/extras/ScalaConcurrentDurationConversionsSpec.scala
@@ -16,31 +16,44 @@ import scala.concurrent.duration.{Duration => SDuration, FiniteDuration => SFini
 class ScalaConcurrentDurationConversionsSpec extends AnyFlatSpec with ScalaCheckDrivenPropertyChecks {
 
   "Converting an SDuration to a JDuration" should "obey the round trip law" in forAll { sDuration: SDuration =>
-    val maybeJDuration: Option[JDuration] = sDurationToJDuration(sDuration)
+    val jDurationOrError = sDurationToJDuration(sDuration)
 
-    maybeJDuration match {
-      case Some(jDuration) => {
+    jDurationOrError match {
+      case Right(jDuration) => {
         val resultSDuration = jDurationToSDuration(jDuration)
           .getOrElse(fail("Duration exceeded bounds on way back in round trip"))
 
         assert(resultSDuration === sDuration)
       }
-      case None => assert(!sDuration.isFinite, "A finite SDuration could not be converted to a JDuration")
+      case Left(e: Errors.ScalaConcurrentDurationIsInfinite) => assert(e.sDuration === sDuration, "A finite SDuration could not be converted to a JDuration")
     }
   }
 
-  it should "return None for an infinite SDuration" in {
-    val genInfiniteDuration: Gen[SDuration.Infinite] = Gen.oneOf(SDuration.Undefined, SDuration.Inf, SDuration.MinusInf)
-
-    forAll(genInfiniteDuration) { sInfiniteDuration: SDuration.Infinite =>
-      assert(sDurationToJDuration(sInfiniteDuration) === None)
+  it should "fail for an infinite SDuration" in forAll(genInfiniteDuration) { sInfiniteDuration: SDuration.Infinite =>
+      assert(sDurationToJDuration(sInfiniteDuration).left.map(_.sDuration) === Left(sInfiniteDuration))
     }
-  }
+
 
   it should "return Some for a finite SDuration" in forAll { sFiniteDuration: SFiniteDuration =>
     val jDuration = sDurationToJDuration(sFiniteDuration).getOrElse(fail)
 
     assertSameNanos(jDuration, sFiniteDuration)
+  }
+
+  "The total conversion from an SDuration to a JDuration" should "convert finite SDurations to a matching SDuration" in forAll { sFiniteDuration: SFiniteDuration =>
+    assertSameNanos(sDurationToJDurationTotal(sFiniteDuration), sFiniteDuration)
+  }
+
+  it should "convert a positive infinite SDuration to the maximum JDuration" in {
+    assert(sDurationToJDurationTotal(SDuration.Inf) === JDuration.ofSeconds(Long.MaxValue, NANOS_PER_SECOND - 1))
+  }
+
+  it should "convert an undefined SDuration to the maximum JDuration" in {
+    assert(sDurationToJDurationTotal(SDuration.Undefined) === JDuration.ofSeconds(Long.MaxValue, NANOS_PER_SECOND - 1))
+  }
+
+  it should "convert a negative infinite SDuration to the minimum JDuration" in {
+    assert(sDurationToJDurationTotal(SDuration.MinusInf) === JDuration.ofSeconds(Long.MinValue, 0))
   }
 
   "Converting an SFiniteDuration to a JDuration" should "obey the round trip law" in forAll {
@@ -58,26 +71,25 @@ class ScalaConcurrentDurationConversionsSpec extends AnyFlatSpec with ScalaCheck
     }
   }
 
-  it should "fail if it exceeds the maximum SDuration" in {
-    val genLargePositiveJDuration =
-      Gen.choose[JDuration](LARGEST_S_DURATION_AS_J_DURATION, JDuration.ofSeconds(Long.MaxValue, NANOS_PER_SECOND - 1))
-    val genLargeNegativeJDuration =
-      Gen.choose[JDuration](JDuration.ofSeconds(Long.MinValue, 0), LARGEST_S_DURATION_AS_J_DURATION.negated)
-
-    val genLargeJDuration = Gen.oneOf(genLargePositiveJDuration, genLargeNegativeJDuration)
-
-    forAll(genLargeJDuration) { largeJDuration =>
+  it should "fail if it exceeds the maximum SDuration" in forAll(genLargeJDuration) { largeJDuration =>
       assert(jDurationToSDuration(largeJDuration).left.map(_.jDuration) === Left(largeJDuration))
     }
-  }
 
-  it should "be precise down to the nanosecond if it fits in an SDuration" in {
-    val genSmallJDuration =
-      Gen.choose[JDuration](LARGEST_S_DURATION_AS_J_DURATION.negated, LARGEST_S_DURATION_AS_J_DURATION)
 
-    forAll(genSmallJDuration) { smallJDuration =>
+  it should "be precise down to the nanosecond if it fits in an SDuration" in forAll(genSmallJDuration) { smallJDuration =>
       assertSameNanos(smallJDuration, jDurationToSDuration(smallJDuration).getOrElse(fail))
     }
+
+  "The total conversion from a JDuration to an SDuration" should "convert small JDurations to a matching SFiniteDuration" in forAll(genSmallJDuration) { smallJDuration =>
+    assertSameNanos(smallJDuration, jDurationToSDurationTotal(smallJDuration))
+  }
+
+  it should "convert large positive JDurations a positive infinite SDuration" in forAll(genLargePositiveJDuration) { largePositiveJDuration =>
+    assert(jDurationToSDurationTotal(largePositiveJDuration) === SDuration.Inf)
+  }
+
+  it should "convert large negative JDurations to a negative infinite SDuration" in forAll(genLargeNegativeJDuration) { largeNegativeJDuration =>
+    assert(jDurationToSDurationTotal(largeNegativeJDuration) === SDuration.Inf)
   }
 
 }
@@ -88,6 +100,18 @@ object ScalaConcurrentDurationConversionsSpec {
 
   private val LARGEST_S_DURATION_AS_J_DURATION: JDuration =
     JDuration.ofSeconds(Long.MaxValue / NANOS_PER_SECOND, Long.MaxValue % NANOS_PER_SECOND)
+
+  private val genSmallJDuration: Gen[JDuration] = Gen.choose[JDuration](LARGEST_S_DURATION_AS_J_DURATION.negated, LARGEST_S_DURATION_AS_J_DURATION)
+
+  private val genLargePositiveJDuration: Gen[JDuration] =
+    Gen.choose[JDuration](LARGEST_S_DURATION_AS_J_DURATION, JDuration.ofSeconds(Long.MaxValue, NANOS_PER_SECOND - 1))
+
+  private val genLargeNegativeJDuration: Gen[JDuration] =
+    Gen.choose[JDuration](JDuration.ofSeconds(Long.MinValue, 0), LARGEST_S_DURATION_AS_J_DURATION.negated)
+
+  private val genLargeJDuration: Gen[JDuration] = Gen.oneOf(genLargePositiveJDuration, genLargeNegativeJDuration)
+
+  private val genInfiniteDuration: Gen[SDuration.Infinite] = Gen.oneOf(SDuration.Undefined, SDuration.Inf, SDuration.MinusInf)
 
   private def assertSameNanos(jDuration: JDuration, sDuration: SDuration): Assertion = {
     if (jDuration.isNegative && sDuration.lt(SDuration.Zero)) return assertSameNanos(jDuration.negated, -sDuration)
